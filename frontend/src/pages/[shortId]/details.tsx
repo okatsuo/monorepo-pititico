3c9fd936ecb89a4eb62093d22f140d0953b09acf
okatsuo <rafael.goncanna@gmail.com>
2022-12-10 22:20:21 -0300

    add popover when copy the the original link

--- frontend/src/pages/[shortId]/details.tsx
+++ frontend/src/pages/[shortId]/details.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/client"
 import { ShortUrlDetailsQueryDocument } from "../../../generated/graphql/graphql"
 import { Navbar } from "../../components/navbar"
 import { Footer } from "../../components/footer"
-import { Container, Card, Grid, Text, Link } from '@nextui-org/react'
+import { Container, Card, Grid, Text, Link, Popover } from '@nextui-org/react'
 
 const ShortIdDetails = () => {
   const router = useRouter()
@@ -26,6 +26,15 @@ const ShortIdDetails = () => {
   if (!data) return <></>
   const { shortUrlDetails } = data
 
+  const copyToClipboard = (str: string) => {
+    var input = document.createElement('input');
+    input.setAttribute('value', str);
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  }
+
   return (
     <span style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100vh' }}>
       <Navbar />
@@ -35,8 +44,15 @@ const ShortIdDetails = () => {
             <Card css={{ padding: '$15', gap: '$2' }} variant="shadow">
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Text b>Link encurtado:</Text>
-                <Link href={`${origin}/${shortUrlDetails.short}`}><Text b color="inherit">{`${origin}/${shortUrlDetails.short}`}</Text></Link>
-                <Link><Text color="inherit">Copiar link original</Text></Link>
+                <Link block isExternal href={`${origin}/${shortUrlDetails.short}`} target="_blank"><Text b color="inherit">{`${origin}/${shortUrlDetails.short}`}</Text></Link>
+                <Popover isBordered>
+                  <Popover.Trigger>
+                    <Link onClick={() => copyToClipboard(shortUrlDetails.url)}><Text color="inherit">Copiar link original</Text></Link>
+                  </Popover.Trigger>
+                  <Popover.Content>
+                    <Text css={{ p: "$2" }}>Link copiado!</Text>
+                  </Popover.Content>
+                </Popover>
               </div>
 
               <div style={{ marginTop: '25px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
