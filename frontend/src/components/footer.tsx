fc76a9fb4be7e9fb20ae44c7e376b7a02803103b
okatsuo <rafael.goncanna@gmail.com>
2022-12-11 19:36:51 -0300

    fixing ads

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,27 +1,10 @@
 import { Navbar as NextUiFooter } from '@nextui-org/react';
-import { GoogleAdsenseContainer } from './adSense';
 
 export const Footer = () => {
   return (
     <NextUiFooter>
       <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', minWidth: '320px' }}>
-        <GoogleAdsenseContainer />
       </span>
     </NextUiFooter>
   )
 }
-
-/*
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5131740489376360"
-     crossorigin="anonymous"></script>
-<!-- ad-footer -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5131740489376360"
-     data-ad-slot="9586280246"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-*/
